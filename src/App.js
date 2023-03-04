021444799a60d06cdb50753071d4ffbdc07832c1
Ishmam Ahmed <ishmam.ahmed24@gmail.com>
2023-03-04 15:45:14 +0000

    Fixed Router Issue(s)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom"
+import Login from "./pages/login"
+import Register from "./pages/register"
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        {/* <Route path="/" element={ <Home/> } /> */}
+        <Route path="login" element={ <Login/> } />
+        <Route path="register" element={ <Register/> } />
+      </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
