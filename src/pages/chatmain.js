449b50283cc51105b508a25a9293d8786712c677
Ishmam Ahmed <ishmam.ahmed24@gmail.com>
2023-03-20 19:14:06 +0000

    Message Box

--- src/pages/chatmain.js
+++ src/pages/chatmain.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useMutation, gql } from "@apollo/client";
 
 function ChatMain() {
   return (
@@ -10,8 +12,16 @@ function ChatMain() {
       <div className="flex w-full lg:w-5/6 lg:h-screen lg:mx-auto lg:my-auto shadow-md">
         {/* Messages */}
         <p className="font-black mt-4 mb-2 pl-4 text-2xl">Insert Username</p>
+        <div class="hidden lg:col-span-2 lg:block">
+          <div class="w-full">
+            <input type="text" placeholder="Message"
+                    class="block w-full py-2 pl-4 mx-3 bg-gray-100 rounded-full outline-none focus:text-gray-700"
+                    name="message" required />
+          </div>
+        </div>
+
       </div>
-    </div>
+    </div> 
   );
 }
 
