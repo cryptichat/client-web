2a4c479b3d2a5019b4ef4a68dbae843b35d34eba
Ishmam Ahmed <ishmam.ahmed24@gmail.com>
2023-03-20 16:53:56 +0000

    Border betn convo list and convo ui

--- src/pages/chatmain.js
+++ src/pages/chatmain.js
@@ -3,7 +3,7 @@ import React from 'react'
 function ChatMain() {
   return (
     <div className='grid grid-cols-6 h-full'>
-        <div className='border-r-neutral-50 col-span-1 h-100'>smol boi</div>
+        <div className='border border-white border-t-0 border-l-0 col-span-1 h-100'>smol boi</div>
         <div className='col-span-5'>big boi</div>
     </div>
   )
