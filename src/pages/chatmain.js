5de810c25816e6f77d69e78a5d2f9e8da404c70b
Karthikeyan Arumugam <kamtechie@gmail.com>
2023-03-20 17:20:28 +0000

    redid layout scaffolding

--- src/pages/chatmain.js
+++ src/pages/chatmain.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import React from "react";
 
 function ChatMain() {
   return (
-    <div className='grid grid-cols-6 h-full'>
-        <div className='border border-white border-t-0 border-l-0 col-span-1 h-100'>smol boi</div>
-        <div className='col-span-5'>big boi</div>
+    <div className="flex w-screen main-chat lg:h-screen divide-solid">
+      <div className="flex flex-col flex-grow lg:max-w-full border border-white border-t-0 border-l-0 border-b-0">
+        {/* Convo list */}
+        <p className="font-black mt-4 mb-2 pl-4 text-2xl">Conversations</p>
+      </div>
+      <div className="flex w-full lg:w-5/6 lg:h-screen lg:mx-auto lg:my-auto shadow-md">
+        {/* Messages */}
+        <p className="font-black mt-4 mb-2 pl-4 text-2xl">Insert Username</p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default ChatMain
+export default ChatMain;
