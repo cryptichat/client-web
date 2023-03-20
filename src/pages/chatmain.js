d31b026ca62bbae0a8e0dc1cc4ba2f78b6c7f342
Karthikeyan Arumugam <kamtechie@gmail.com>
2023-03-20 16:37:57 +0000

    basic column ui

---
+++ src/pages/chatmain.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function ChatMain() {
+  return (
+    <div className='grid grid-cols-6 h-full'>
+        <div className='border-r-neutral-50 col-span-1 h-100'>smol boi</div>
+        <div className='col-span-5'>big boi</div>
+    </div>
+  )
+}
+
+export default ChatMain
