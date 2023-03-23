57a56a53e44d6ca31863082badc55a6be0101c52
tobiy105 <tobiy105@gmail.com>
2023-03-23 19:59:53 +0000

    Fixed bugs

--- src/pages/friends.js
+++ src/pages/friends.js
@@ -10,24 +10,47 @@ const GET_CONV = gql`
 
 const GET_USERS = gql`
     query ConversationParticipants($conversationId: Int!, $token: String!){
-      conversationParticipants(conversationId: $conversationId,token: $token)
+      conversationParticipants(conversationId: $conversationId,token: $token){
+        username,
+        passwordHash,
+        publicKey
+      }
+      
      }
 `;
 
 const  Friends = () => {
     let get_token = localStorage.getItem("auth-token");
     let get_id = localStorage.getItem("conversationId");
-    // const users = useQuery(GET_USERS);
-    const { loading, error, data } = useQuery(GET_CONV, {
+    const users = useQuery(GET_USERS);
+    const { loading:conv_loading, error:conv_error, data:conv_data } = useQuery(GET_CONV, {
         variables: { nConversations:10,token:get_token },
     });
-    if (loading) return null;
-    if (error) return `Error! ${error}`;
+    const { loading:user_loading, error:user_error, data:user_data } = useQuery(GET_USERS, {
+      variables: { conversationId:1,token:get_token },
+    });
+    if (conv_loading) return null;
+    if (conv_error) return `Error! ${conv_error}`;
+
+    if (conv_data) {
+    
+    if (user_loading) return null;
+    if (user_error) return `Error! ${user_error}`;
+    console.log(user_data.conversationParticipants[0])
+    var x = user_data.conversationParticipants[0]
+    var vals = Object.keys(x).sort().reduce(function (arr, key) {
+      arr = arr.concat(x[key]);
+      return arr;
+    }, []).join('\n');
+    console.log(vals )
   return (
     <div>
-      {data.conversationsByUser}
+      {conv_data.conversationsByUser}
+      {/* {user_data.conversationParticipants[0]} */}
+      {/* {username} */}
+      {vals}
     </div>
   )
-}
+}}
 
 export default Friends
