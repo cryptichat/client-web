36ebfcfaa4fef9cab2bc242f83c0156fb4f5a26c
tobiy105 <tobiy105@gmail.com>
2023-03-23 18:24:23 +0000

    added conversationParticipants query

--- src/pages/friends.js
+++ src/pages/friends.js
@@ -2,9 +2,15 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useQuery, gql } from "@apollo/client";
 
-const GET_USERS = gql`
+const GET_CONV = gql`
     query ConversationsByUser($nConversations: Int!, $token: String!){
         conversationsByUser(nConversations: $nConversations,token: $token)
+     }
+`;
+
+const GET_USERS = gql`
+    query ConversationParticipants($conversationId: Int!, $token: String!){
+      conversationParticipants(conversationId: $conversationId,token: $token)
      }
 `;
 
@@ -12,7 +18,7 @@ const  Friends = () => {
     let get_token = localStorage.getItem("auth-token");
     let get_id = localStorage.getItem("conversationId");
     // const users = useQuery(GET_USERS);
-    const { loading, error, data } = useQuery(GET_USERS, {
+    const { loading, error, data } = useQuery(GET_CONV, {
         variables: { nConversations:10,token:get_token },
     });
     if (loading) return null;
