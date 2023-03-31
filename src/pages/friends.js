ce156db614a4758f74d55c55f3a6b22014b4202f
tobiy105 <tobiy105@gmail.com>
2023-03-31 17:52:01 +0100

    Added array to users

--- src/pages/friends.js
+++ src/pages/friends.js
@@ -22,13 +22,12 @@ const  Friends = () => {
     const navigate = useNavigate();
     let get_token = localStorage.getItem("auth-token");
     const [errors, setErrors] = useState([]);
+    let arr = [];
     let user = "";
     const [GetUsers] = useLazyQuery(GET_USERS, {
 
       fetchPolicy:'cache-and-network',
-      // fetchPolicy: "no-cache",
       onCompleted: ( GetUsers ) => {
-        console.log(GetUsers.conversationParticipants[0].username)
         user = GetUsers.conversationParticipants[0].username;
         getValues();
       },
@@ -43,44 +42,44 @@ const  Friends = () => {
         variables: { nConversations:10,token:get_token },
         fetchPolicy:'cache-and-network', 
     });
-    let arr = [1,2,5];
-    useEffect(async () => {
-      if (conv_data) {
-        console.log(conv_data);
-        let length = conv_data['conversationsByUser'].length;
-        console.log(length);
-
-        for(var i = 0; i < length; i++){
-          
-          const res = await GetUsers({ variables: { conversationId:arr[i],token:get_token },
-            fetchPolicy: 'cache-and-network',
-            notifyOnNetworkStatusChange: true,
-          });
-          console.log(conv_data['conversationsByUser'][i]);
-          console.log("res");
-          console.log(res);
+    
+    useEffect( () => {
+      {
+        async function  func() {
+          if (conv_data) {
+    
+            for(var i = 0; i < conv_data['conversationsByUser'].length; i++){
+              
+              const res = await GetUsers({ variables: { conversationId:conv_data['conversationsByUser'][i],token:get_token },
+                fetchPolicy: 'cache-and-network',
+                notifyOnNetworkStatusChange: true,
+              });
+              
+              console.log(res.data['conversationParticipants'][0]['username']);
+              arr.push(res.data['conversationParticipants'][0]['username']);
+              console.log(arr);
+            }
+          }
         }
+        func();
+      
       }
-    }, [conv_data]);
-
+    }, [conv_data]);
+   
     function getValues() {
       document.getElementById("result").innerHTML += user + "\n" +"</pre>";
     }
    
-  
   return (
     <div>
       
       {conv_loading && <p>Loading...</p>}
       {conv_error && <p>Error! {conv_error}</p>}
-      {!conv_loading && !conv_error && <p>Content goes here...</p> && conv_data.conversationsByUser[0]}
+      {!conv_loading && !conv_error && <p>Content goes here...</p> && conv_data.conversationsByUser[0]}
     
-  
       <div id="result" ></div>
       
     </div>
-   
-    
   )
 };
 
