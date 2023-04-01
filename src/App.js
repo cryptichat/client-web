f3d5bea1acfab7d921e8347d868d69984188ec4f
Karthikeyan Arumugam <kamtechie@gmail.com>
2023-04-01 18:52:38 +0100

    redirects to login screen if not logged in
    Co-authored-by: tobiy105 <tobiy105@users.noreply.github.com>
    Co-authored-by: Ish <IshmamDC217@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import { Routes, Route } from "react-router-dom"
-import Login from "./pages/login"
-import Register from "./pages/register"
-import ChatMain from "./pages/chatmain"
+import { Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/login";
+import Register from "./pages/register";
+import ChatMain from "./pages/chatmain";
+
+const loggedIn = localStorage.getItem("auth-token") ? true : false;
 
 function App() {
   return (
     <div className="App h-screen">
       <Routes>
-        <Route path="/" element={ <ChatMain/> } />
-        <Route path="login" element={ <Login/> } />
-        <Route path="register" element={ <Register/> } />
+        <Route
+          path="/"
+          element={loggedIn ? <ChatMain /> : <Navigate replace to={"/login"} />}
+        />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
       </Routes>
     </div>
-  )
-} 
+  );
+}
 
-export default App
+export default App;
