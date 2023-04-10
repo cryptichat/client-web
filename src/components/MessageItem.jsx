8aea4e33494898dc9f28433cbb41d3823e4571dc
Karthikeyan Arumugam <kamtechie@gmail.com>
2023-04-10 23:56:57 +0100

    Ui animations

--- src/components/MessageItem.jsx
+++ src/components/MessageItem.jsx
@@ -1,15 +1,20 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 function MessageItem({ message }) {
   const isSentByCurrentUser =
     message.sender.username === localStorage.getItem("dsmessenger-username");
   return (
-    <div
+    <motion.div
       className={`${
         isSentByCurrentUser ? "bg-[#8b5cf6] rounded-[15px]" : "bg-neutral-800 rounded-[15px] "
       } min-w-[7%] w-fit p-3 my-2 rounded-lg ${
         isSentByCurrentUser ? "ml-auto" : "mr-auto"
       }`}
+      initial={{ scale: 0 }}
+      animate={{ scale: 1 }}
+      exit={{ scale: 0 }}
+      transition={{ duration: 0.3 }}
     >
       <p
         className={`text-${isSentByCurrentUser ? "white" : "gray-800"} text-sm`}
@@ -17,7 +22,7 @@ function MessageItem({ message }) {
         <b>{message.sender.username}</b>
         <p>{message.content}</p>
       </p>
-    </div>
+    </motion.div>
   );
 }
 
--- src/components/convoListItem.jsx
+++ src/components/convoListItem.jsx
@@ -1,18 +1,23 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 function ConvoListItem({ username, key, onClick, active }) {
   return (
-    <li
+    <motion.li
       key={key}
       className={`w-full flex justify-start pl-1 py-2 hover:bg-slate-600 ${
         active ? "bg-slate-600" : null
       } cursor-pointer rounded-md`}
       onClick={onClick}
+      initial={{ x: -50, opacity: 0 }}
+      animate={{ x: 0, opacity: 1 }}
+      exit={{ x: 50, opacity: 0 }}
+      transition={{ duration: 0.3 }}
     >
       <div className="flex items-center">
         <p className="w-36 truncate text-left">{username}</p>
       </div>
-    </li>
+    </motion.li>
   );
 }
 
