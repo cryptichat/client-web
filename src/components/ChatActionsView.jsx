badc61acbced1cc211fb5cd45893d22b452b21e9
Karthikeyan Arumugam <kamtechie@gmail.com>
2023-04-10 23:20:19 +0100

    split out left bar into own component

---
+++ src/components/ChatActionsView.jsx
@@ -0,0 +1,108 @@
+import React, { useState } from "react";
+import ConvoListItem from "../components/convoListItem";
+
+import { BiMessageRoundedAdd } from "react-icons/bi";
+import { BiLogOut } from "react-icons/bi";
+
+export default function ChatActionsView({
+  loggedInUsername,
+  activeConvo,
+  setActiveConvo,
+  userConversations,
+  createConvo,
+}) {
+  const [addChatOpen, setAddChatOpen] = useState(false);
+  const [createConvoText, setCreateConvoText] = useState("");
+
+  function handleLogout() {
+    localStorage.removeItem("auth-token");
+    localStorage.removeItem("dsmessenger-username");
+    navigate("/login");
+  }
+
+  return (
+    <div className="flex flex-col flex-grow lg:max-w-full border border-white border-t-0 border-l-0 border-b-0">
+      {/* Convo list */}
+      <div className="flex items-center justify-between">
+        <p className="font-black mt-4 mb-3 pl-4 text-2xl">Conversations</p>
+        <div
+          className={`p-2 rounded mt-1 mr-2 cursor-pointer hover:bg-slate-600 ${
+            addChatOpen && "bg-slate-600"
+          }`}
+        >
+          <BiMessageRoundedAdd
+            size={24}
+            onClick={() => setAddChatOpen(!addChatOpen)}
+          />
+        </div>
+      </div>
+      {addChatOpen && (
+        <div className="bg-slate-600 py-2 mb-2">
+          <input
+            type="text"
+            placeholder="Enter username"
+            className="mt-1 py-4 pl-4 mx-3 bg-gray-100 rounded-[10px] outline-none focus:text-gray-700"
+            style={{ width: "-webkit-fill-available" }}
+            name="message"
+            onChange={(e) => setCreateConvoText(e.target.value)}
+            required
+          />
+          <a
+            href="#"
+            className="hidden bg-[#8b5cf6] md:flex border border-[#000000] p-2 mx-2 mt-2 mb-1
+                              text-[#ffffff] rounded-[10px] items-center gap-2
+                                hover:bg-[#4c1d95] hover:text-white transition duration-200"
+            onClick={() => {
+              console.log("add user to convo", createConvoText);
+              createConvo({
+                variables: {
+                  directMessage: true,
+                  token: localStorage.getItem("auth-token"),
+                  users: [loggedInUsername, createConvoText],
+                  keys: ["XXX", "XXX"],
+                },
+              });
+            }}
+          >
+            Start
+          </a>
+        </div>
+      )}
+
+      <div className="hidden lg:block pl-4 pr-4 text-white hover:rounded-md">
+        <ul className="divide-y divide-gray-300 truncate">
+          {userConversations.map((convo) => {
+            return (
+              <ConvoListItem
+                username={convo.user}
+                active={activeConvo && activeConvo.conv_id === convo.conv_id}
+                onClick={() => setActiveConvo(convo)}
+              />
+            );
+          })}
+        </ul>
+      </div>
+
+      <div className="grow"></div>
+      <div
+        className="flex py-4 items-center mb-3.5"
+        style={{ display: "flex", justifyContent: "center" }}
+      >
+        <a
+          href="#"
+          className="hidden bg-zinc-900 md:flex
+                              text-[#ffffff] rounded-[10px] items-center gap-2
+                                hover:bg-zinc-900 hover:text-white transition duration-200"
+        >
+          <p
+            className="flex px-3 scale-90 hover:scale-100 ease-in duration-200"
+            onClick={handleLogout}
+          >
+            <BiLogOut className="text-[25px] mr-2" />
+            {loggedInUsername}
+          </p>
+        </a>
+      </div>
+    </div>
+  );
+}
--- src/pages/chatmain.jsx
+++ src/pages/chatmain.jsx
@@ -1,9 +1,7 @@
 import React, { useState, useEffect } from "react";
-import ConvoListItem from "../components/convoListItem";
+import ChatActionsView from "../components/ChatActionsView";
 import MessageItem from "../components/MessageItem";
 import { BsFillSendFill } from "react-icons/bs";
-import { BiMessageRoundedAdd } from "react-icons/bi";
-import { BiLogOut } from "react-icons/bi";
 import { CgAttachment } from "react-icons/cg";
 import { useNavigate } from "react-router-dom";
 import { useMutation, useQuery, useLazyQuery, gql } from "@apollo/client";
@@ -98,9 +96,7 @@ const GET_MESSAGE = gql`
 `;
 
 function ChatMain() {
-  const [addChatOpen, setAddChatOpen] = useState(false);
   const [errors, setErrors] = useState([]);
-  const [createConvoText, setCreateConvoText] = useState("");
   const [userConversations, setUserConversations] = useState([]);
   const [activeConvo, setActiveConvo] = useState({});
   const [activeMessages, setActiveMessages] = useState([]);
@@ -108,11 +104,6 @@ function ChatMain() {
 
   const navigate = useNavigate();
 
-  function handleLogout() {
-    localStorage.removeItem("auth-token");
-    localStorage.removeItem("dsmessenger-username");
-    navigate("/login");
-  }
   const loggedInUsername = localStorage.getItem("dsmessenger-username");
   let get_token = localStorage.getItem("auth-token");
 
@@ -128,7 +119,6 @@ function ChatMain() {
     onError: ({ graphQLErrors }) => {
       console.error(graphQLErrors);
       toast.error("Error creating conversation, please check console");
-
     },
   });
 
@@ -173,7 +163,6 @@ function ChatMain() {
       toast.error("Error sending message, please check console");
     },
   });
-
 
   function handleSendMessage() {
     console.log("message to send: " + messageText);
@@ -268,89 +257,13 @@ function ChatMain() {
   return (
     <div className="flex w-screen main-chat lg:h-screen divide-solid">
       <ToastContainer theme="dark" />
-      <div className="flex flex-col flex-grow lg:max-w-full border border-white border-t-0 border-l-0 border-b-0">
-        {/* Convo list */}
-        <div className="flex items-center justify-between">
-          <p className="font-black mt-4 mb-3 pl-4 text-2xl">Conversations</p>
-          <div
-            className={`p-2 rounded mt-1 mr-2 cursor-pointer hover:bg-slate-600 ${
-              addChatOpen && "bg-slate-600"
-            }`}
-          >
-            <BiMessageRoundedAdd
-              size={24}
-              onClick={() => setAddChatOpen(!addChatOpen)}
-            />
-          </div>
-        </div>
-        {addChatOpen && (
-          <div className="bg-slate-600 py-2 mb-2">
-            <input
-              type="text"
-              placeholder="Enter username"
-              className="mt-1 py-4 pl-4 mx-3 bg-gray-100 rounded-[10px] outline-none focus:text-gray-700"
-              style={{ width: "-webkit-fill-available" }}
-              name="message"
-              onChange={(e) => setCreateConvoText(e.target.value)}
-              required
-            />
-            <a
-              href="#"
-              className="hidden bg-[#8b5cf6] md:flex border border-[#000000] p-2 mx-2 mt-2 mb-1
-                                  text-[#ffffff] rounded-[10px] items-center gap-2
-                                    hover:bg-[#4c1d95] hover:text-white transition duration-200"
-              onClick={() => {
-                console.log("add user to convo", createConvoText);
-                CreateConvoHandler({
-                  variables: {
-                    directMessage: true,
-                    token: localStorage.getItem("auth-token"),
-                    users: [loggedInUsername, createConvoText],
-                    keys: ["XXX", "XXX"],
-                  },
-                });
-              }}
-            >
-              Start
-            </a>
-          </div>
-        )}
-
-        <div className="hidden lg:block pl-4 pr-4 text-white hover:rounded-md">
-          <ul className="divide-y divide-gray-300 truncate">
-            {userConversations.map((convo) => {
-              return (
-                <ConvoListItem
-                  username={convo.user}
-                  active={activeConvo && activeConvo.conv_id === convo.conv_id}
-                  onClick={() => setActiveConvo(convo)}
-                />
-              );
-            })}
-          </ul>
-        </div>
-
-        <div className="grow"></div>
-        <div
-          class="flex py-4 items-center mb-3.5"
-          style={{ display: "flex", justifyContent: "center" }}
-        >
-          <a
-            href="#"
-            className="hidden bg-zinc-900 md:flex
-                                  text-[#ffffff] rounded-[10px] items-center gap-2
-                                    hover:bg-zinc-900 hover:text-white transition duration-200"
-          >
-            <p
-              className="flex px-3 scale-90 hover:scale-100 ease-in duration-200"
-              onClick={handleLogout}
-            >
-              <BiLogOut className="text-[25px] mr-2" />
-              {loggedInUsername}
-            </p>
-          </a>
-        </div>
-      </div>
+      <ChatActionsView
+        loggedInUsername={loggedInUsername}
+        activeConvo={activeConvo}
+        setActiveConvo={setActiveConvo}
+        userConversations={userConversations}
+        createConvo={CreateConvoHandler}
+      />
       <div className="flex flex-col w-full lg:w-5/6 lg:h-screen lg:mx-auto lg:my-auto shadow-md">
         {/* Messages */}
         <div>
@@ -360,10 +273,10 @@ function ChatMain() {
         </div>
         <div className="grow px-4">
           {activeMessages.map((message) => (
-            <MessageItem message={message}/>
+            <MessageItem message={message} />
           ))}
         </div>
-        <div class="flex py-4 items-center">
+        <div className="flex py-4 items-center">
           <form className="inputContainer flex-1 py-2">
             <input
               type="text"
