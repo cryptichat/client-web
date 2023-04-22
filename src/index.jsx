3fb06a31f6c551c5126bafc5aec786f442514bfc
Karthikeyan Arumugam <kamtechie@gmail.com>
2023-04-22 01:47:19 +0100

    added websocket link

--- src/index.jsx
+++ src/index.jsx
@@ -4,10 +4,33 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { ApolloClient, InMemoryCache, ApolloProvider, split, HttpLink } from "@apollo/client";
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
+import { createClient } from 'graphql-ws';
+import { getMainDefinition } from '@apollo/client/utilities';
+
+const httpLink = new HttpLink({
+  uri: 'http://localhost:5000/graphql'
+});
+
+const wsLink = new GraphQLWsLink(createClient({
+  url: 'ws://localhost:5000/graphql/subscriptions',
+}));
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink,
+);
 
 const client = new ApolloClient({
-  uri: "http://127.0.0.1:5000/graphql",
+  link: splitLink,
   cache: new InMemoryCache(),
 });
 
