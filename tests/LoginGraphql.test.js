bdc63ae17dcbf5a9278f948175c63506b09c6f23
tobiy105 <tobiy105@gmail.com>
2023-04-23 06:25:18 +0100

    create user for login

--- tests/LoginGraphql.test.js
+++ tests/LoginGraphql.test.js
@@ -2,6 +2,35 @@ import '@testing-library/jest-dom/extend-expect';
 import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 import { gql } from "@apollo/client";
 import fetch from 'cross-fetch';
+import crypto from 'crypto';
+
+function arrayBufferToBase64(buffer) {
+  let binary = '';
+  const bytes = new Uint8Array(buffer);
+  const len = bytes.byteLength;
+  for (let i = 0; i < len; i++) {
+    binary += String.fromCharCode(bytes[i]);
+  }
+  return btoa(binary);
+}
+
+const SIGNUP = gql`
+  mutation CreateAccount(
+    $username: String!
+    $email: String!
+    $password: String!
+    $publicKey: String!
+  ) {
+    createAccount(
+      username: $username
+      email: $email
+      password: $password
+      publickey: $publicKey
+    ) {
+      accessToken
+    }
+  }
+`;
 
 const LOGIN = gql`
   mutation Login($username: String!, $password: String!) {
@@ -20,11 +49,46 @@ describe("Login mutation", () => {
     });
   
     it("should return an access token on successful register", async () => {
-   
+      
+        // Generate key pair
+      const keyPair = await crypto.subtle.generateKey(
+        {
+          name: "RSA-OAEP",
+          modulusLength: 2048,
+          publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
+          hash: "SHA-256",
+        },
+        true,
+        ["encrypt", "decrypt"]
+      );
+
+      // Export public key
+      const publicKeyDer = await crypto.subtle.exportKey(
+        "spki",
+        keyPair.publicKey
+      );
+      const publicKeyPem = arrayBufferToBase64(publicKeyDer);
+
+      // Export private key
+      const privateKeyDer = await crypto.subtle.exportKey(
+        "pkcs8",
+        keyPair.privateKey
+      );
+
+      const { reg } = await client.mutate({
+        mutation: SIGNUP,
+        variables: {
+          username: "test2",
+          email: "test2@example.com",
+          password: "password123",
+          publicKey: publicKeyPem,
+        },
+      });
+
       const { data } = await client.mutate({
         mutation: LOGIN,
         variables: {
-          username: "user_2",
+          username: "test2",
           password: "password123",   
         },
       });
