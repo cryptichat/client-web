07dd13e6c9dc12fc9f9cbb4a5751d9a46f081508
Ben O'Hara <benohara132@gmail.com>
2023-04-28 11:28:59 +0100

    might be a fix to signup redirect

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -125,6 +125,7 @@ function Register() {
 
         localStorage.setItem("privateKey:" + formState.uname, arrayBufferToBase64(privateKeyDer));
         console.log("Private key stored securely in local storage");
+        window.location.href = window.location.hostname;
       } catch (error) {
         console.error(error);
       }
