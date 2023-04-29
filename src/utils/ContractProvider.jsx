2d46a9131eef196e4637cb86013381f298ae505f
Ben O'Hara <benohara132@gmail.com>
2023-04-29 15:25:54 +0100

    lol v69

--- src/utils/ContractProvider.jsx
+++ src/utils/ContractProvider.jsx
@@ -3,6 +3,11 @@ import Web3 from "web3";
 
 const ContractContext = createContext();
 
+
+const providerURL = import.meta.env.VITE_WEB3_PROVIDER_URL || "http://localhost:8545";
+const contractAddress = import.meta.env.VITE_DEPLOYED_CONTRACT_ADDRESS; //|| data.networks["5777"].address;
+
+
 const ContractProvider = ({ children }) => {
   const [web3, setWeb3] = useState(null);
   const [contract, setContract] = useState(null);
@@ -12,13 +17,11 @@ const ContractProvider = ({ children }) => {
       //const response = await fetch("/contract.json");
       //const data = await response.json();
       const abi = await (await fetch('/abi.json')).json();
-      const providerURL = import.meta.env.VITE_WEB3_PROVIDER_URL || "http://localhost:8545";
-      console.log(import.meta.env.VITE_GRAPHQL_URL);
+      //      console.log(import.meta.env.VITE_GRAPHQL_URL);
       console.log("providerURL", providerURL);
       const web3 = new Web3(providerURL);
       setWeb3(web3);
       const contractABI = abi.abi; //|| data.abi;
-      const contractAddress = import.meta.env.VITE_DEPLOYED_CONTRACT_ADDRESS; //|| data.networks["5777"].address;
       console.log("contractAddress", contractAddress);
       const contract = new web3.eth.Contract(contractABI, contractAddress);
       setContract(contract);
